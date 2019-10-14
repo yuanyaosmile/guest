030a8790c9a0237acb067a95a0ebd83d868f8de6
yuanyao <yuanyaosmile@gmail.com>
2019-10-14 20:55:38 +0800

    add index.html and some libs

--- src/main/resources/static/js/guest.js
+++ src/main/resources/static/js/guest.js
@@ -1,5 +1,10 @@
-var myApp = angular.module('myApp', ['ngMaterial']);
+var myApp = angular.module('myApp', ['ngMaterial', 'wui.date']);
 myApp.controller('uploadController', function ($scope) {
+    $scope.upload = "upload";
+    $scope.yy = "yuanyao";
+});
+
+myApp.controller('DemoController', function ($scope) {
     $scope.upload = "upload";
     $scope.yy = "yuanyao";
 });
