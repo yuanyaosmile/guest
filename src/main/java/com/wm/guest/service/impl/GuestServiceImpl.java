07791582df739ae2a81b88a7f3974d2e1c9e9c78
yuanyao <yuanyaosmile@gmail.com>
2019-10-16 23:56:47 +0800

    implement send email automaticly

--- src/main/java/com/wm/guest/service/impl/GuestServiceImpl.java
+++ src/main/java/com/wm/guest/service/impl/GuestServiceImpl.java
@@ -4,6 +4,8 @@ import com.wm.guest.dao.GuestMapper;
 import com.wm.guest.entity.User;
 import com.wm.guest.service.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +14,9 @@ public class GuestServiceImpl implements GuestService {
     @Autowired
     GuestMapper guestMapper;
 
+    @Autowired
+    JavaMailSender javaMailSender;
+
     @Override
     public User getUser(Long id) {
         return guestMapper.selectByPrimaryKey(id);
@@ -19,6 +24,13 @@ public class GuestServiceImpl implements GuestService {
 
     @Override
     public int addEvent(User user) {
+        SimpleMailMessage mainMessage = new SimpleMailMessage();
+        mainMessage.setFrom("15261852099@163.com");
+        mainMessage.setTo("15261852099@163.com");
+        mainMessage.setSubject("guest");
+        String context = user.getGuest() + "\n" + user.getVisited()+ "\n" +user.getPhone()+ "\n";
+        mainMessage.setText(context);
+        javaMailSender.send(mainMessage);
         return guestMapper.insert(user);
     }
 }
