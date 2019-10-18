430e0c2a6c434b41b83168d4e76cb2dff8fb2e7f
yuanyao <yuanyaosmile@gmail.com>
2019-10-18 22:20:45 +0800

    FE adjust

--- src/main/resources/static/js/guest.js
+++ src/main/resources/static/js/guest.js
@@ -1,13 +1,16 @@
 'use strict'
 var myApp = angular.module('myApp', ['ngMaterial', 'wui.date']);
 
-myApp.controller('DemoController', function ($scope,$http) {
-  $scope.add = function () {
-      console.log($scope.user)
-      $http.post("http://localhost:10086/add",$scope.user).then(function(response){
-        console.log($scope.user);
-      },function (error) {
-          console.log($scope.user);
-      });
-  }
+myApp.controller('DemoController', function ($scope, $http) {
+
+    $scope.add = function () {
+        //$scope.flag = true;
+        console.log($scope.user)
+        $http.post("http://localhost:10086/add", $scope.user).then(function (response) {
+            console.log($scope.user);
+            $scope.flag = true;
+        }, function (error) {
+            console.log($scope.user);
+        });
+    }
 });
